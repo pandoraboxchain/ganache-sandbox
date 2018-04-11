71e96e923cb769867e5e89c274e6553ac91d6807
Kostiantyn Smyrnov <kostysh@gmail.com>
2018-04-11 23:54:12 +0300

    coverage badge

--- tests/tests-runner.sh
+++ tests/tests-runner.sh
@@ -7,5 +7,5 @@ if [ "$WITH_COVERAGE" -eq 0 ]; then
 else 
     echo "Running tests with coverage"
     npx istanbul cover _mocha --report lcovonly -- --require @babel/register -R spec --timeout 70000 ./tests/spec/**/*.test.js
-    cat ./coverage/lcov.info | npx codacy-coverage --token b9f35cad059c44b792590ef78c8b81f8
+    cat ./coverage/lcov.info | npx codacy-coverage --token 3ab75785e85744d6854c9f92c7c98d45
 fi
